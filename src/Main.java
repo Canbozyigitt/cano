587a75496811d5ce4492335470ab840ea4e65c92
Canbozyigitt <136346490+Canbozyigitt@users.noreply.github.com>
2024-02-10 15:13:09 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 6; // C harfinin yüksekliği
+        int n = 4; // C harfinin yüksekliği
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
